457761dcf360595d07280b6eb7f3bac6565c7a3b
goranurukalo <goran.urukalo.117.14@ict.edu.rs>
2018-05-19 20:25:03 +0200

    adding templete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  authSubscription: Subscription;
+
+  constructor(
+    public auth: AuthService
+  ) { }
+
+  ngOnInit() {
+    // Subscribe to login status subject
+    // If authenticated, subscribe to dragons data observable
+    // If not authenticated, unsubscribe from dragons data
+    this.authSubscription = this.auth.loggedIn$
+      .subscribe(loggedIn => {
+        if (loggedIn) {
+          console.log("You are logged in");
+          //
+          // todo
+          //
+        } else {
+          //not logged in
+        }
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    // Unsubscribe from observables
+    this.authSubscription.unsubscribe();
+  }
+
 }
